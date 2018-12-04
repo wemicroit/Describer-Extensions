751b68409f412fe82231fc6b680198149fdec8df
James Thompson <wemicroit@outlook.com>
2018-12-05 00:35:17 +1100

    Rename fixes

--- Describer-TemplatePackage/Properties/AssemblyInfo.cs
+++ Describer-TemplatePackage/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Commenter_ItemTemplates")]
+[assembly: AssemblyTitle("Describer_Templates")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Commenter_ItemTemplates")]
+[assembly: AssemblyCompany("WeMicroIt")]
+[assembly: AssemblyProduct("Describer_Templates")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
